42dde2e1749b875b802e26a0f9aa5b34a1daadac
luisaveronese <luisamoraes2003@gmail.com>
2023-01-30 08:52:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var valorEmDolar = 30;
+var cotacaoDolar = 5.11;
+var valorEmReal = valorEmDolar * cotacaoDolar; 
+valorEmReal = valorEmReal.toFixed(2);
+alert('R$ ' + valorEmReal);
+
+var valorEmEuro = 25;
+var cotacaoEuro = 5.56;
+var valorReal = valorEmEuro * cotacaoEuro;
+valorReal = valorReal.toFixed(2);
+alert('R$ ' + valorReal);
